8a2b498ca10670455972ac67d4b10145e3856fdb
dsalenkoff <dmitrij.salenkoff@gmail.com>
2023-04-26 14:10:18 +0600

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -2,22 +2,48 @@ import json
 
 
 class Offer:
-    def __init__(self, offer_id: str, **kwargs):
-        self.offer_id = offer_id
+    def __init__(self, id: str, **kwargs):
+        self.id = id
         for i in kwargs:
-            if i != 'id':
+            if i != 'id' and i != 'partner_content':
                 setattr(self, i, kwargs[i])
+            elif i == 'partner_content':
+                for j in kwargs['partner_content']:
+                    setattr(self, j, kwargs['partner_content'][j])
 
     def update_offer(self, other):
+        updated_properties = []
         for attribute in other.__dict__:
-            if attribute != 'offer_id':
+            if attribute != 'id' and getattr(self, attribute, None) != getattr(other, attribute, None):
                 setattr(self, attribute, getattr(other, attribute))
+                updated_properties.append(attribute)
+        return updated_properties, self
 
     def get_properties(self):
-        return [i for i in self.__dict__ if i != 'offer_id']
+        return [i for i in self.__dict__.keys() if i != 'id']
 
-    def get_message_dist(self):
-        return self.__dict__
+    def get_message_dict(self, triggers: list, shipments: list):
+        dic = {'id': self.id}
+        lst_to_send = triggers + shipments
+        for key, value in self.__dict__.items():
+            if key in lst_to_send and (key == 'price' or key == 'stock_count'):
+                dic[key] = value
+
+        if 'partner_content' in lst_to_send:
+            for i in ['title', 'description']:
+                if i in self.__dict__.keys():
+                    if 'partner_content' not in dic.keys():
+                        dic['partner_content'] = {i: getattr(self, i)}
+                    else:
+                        dic['partner_content'].update({i: getattr(self, i)})
+
+        for i in ['title', 'description']:
+            if i in lst_to_send and i in self.__dict__.keys():
+                if 'partner_content' not in self.__dict__.keys():
+                    dic['partner_content'] = {i: getattr(self, i)}
+                else:
+                    dic['partner_content'].update({i: getattr(self, i)})
+        return dic
 
 
 class Subscriber:
@@ -31,46 +57,49 @@ class Update:
         self.trace_id = trace_id
         self.offer = offer
 
-    def get_message_json(self):
-        return json.dumps({'trace_id': self.trace_id
-                              , 'offer': self.offer.get_message_dist()})
+    def get_message_json(self, triggers: list, shipments: list):
+        return json.dumps({'trace_id': self.trace_id, 'offer': self.offer.get_message_dict(triggers, shipments)})
 
 
 subscribers = []
 updates = []
+offers = {}
 
-# n, m = list(map(int, input().split(' ')))
-#
-# for _ in range(n):
-#     row = input().split(' ')
-#     a, b = int(row[0]), int(row[1])
-#     subscribers.append(Subscriber(row[2:2 + a], row[2 + a:]))
-#
-# for _ in range(m):
-#     js = json.load(input())
-#     updates.append(Update(js['trace_id'], Offer(**js['offer'])))
+n, m = list(map(int, input().split(' ')))
 
-with open('./test.txt', 'r') as f:
-    rows = f.read().splitlines()
-n, m = list(map(int, rows[0].split(' ')))
-for i in range(1, n + 1):
-    row = rows[i].split(' ')
+for _ in range(n):
+    row = input().split(' ')
     a, b = int(row[0]), int(row[1])
     subscribers.append(Subscriber(row[2:2 + a], row[2 + a:]))
 
-for i in range(m):
-    js = json.loads(rows[n + i + 1])
-    updates.append(Update(js['trace_id'], Offer(js['offer']['id'], **js['offer'])))
+for _ in range(m):
+    inp = input()
+    with io.StringIO(inp) as f:
+        js = json.load(f)
+    updates.append(Update(js['trace_id'], Offer(**js['offer'])))
 
-offers = {}
+# with open('./test.txt', 'r') as f:
+#     rows = f.read().splitlines()
+# n, m = list(map(int, rows[0].split(' ')))
+# for i in range(1, n + 1):
+#     row = rows[i].split(' ')
+#     a, b = int(row[0]), int(row[1])
+#     subscribers.append(Subscriber(row[2:2 + a], row[2 + a:]))
+#
+# for i in range(m):
+#     js = json.loads(rows[n + i + 1])
+#     updates.append(Update(js['trace_id'], Offer(**js['offer'])))
+
 for upd in updates:
     updated_properties = []
-    if upd.offer.offer_id not in offers.keys():
-        offers[upd.offer.offer_id] = upd.offer
+    if upd.offer.id not in offers.keys():
+        offers[upd.offer.id] = upd.offer
         updated_properties += upd.offer.get_properties()
     else:
-        offers[upd.offer.offer_id].update_offer(upd.offer)
-        updated_properties += upd.offer.get_properties()
+        updated_properties, upd.offer = offers[upd.offer.id].update_offer(upd.offer)
     for sub in subscribers:
-        if set(sub.triggers) & set(updated_properties):
-            print(upd.get_message_json())
+        lst_buff = sub.triggers
+        if 'partner_content' in sub.triggers:
+            lst_buff = sub.triggers + ['title', 'description']
+        if set(lst_buff) & set(updated_properties):
+            print(upd.get_message_json(sub.triggers, sub.shipments))
